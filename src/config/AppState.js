fc671b8157102c3c9e6c5e1d76536d08cb7eaca6
azizmashkour <azizmashkour@gmail.com>
2019-02-19 16:59:26 +0100

    fix: update default app state params

--- src/config/AppState.js
+++ src/config/AppState.js
@@ -1,5 +1,5 @@
 export const AppState = {
-  obserViewActive: true,
+  isMouseInside: false,
   showTitle: true,
   openModal: false,
   lineStrokeWidth: 3,
@@ -18,15 +18,14 @@ export const AppState = {
     E: 'E',
   },
   points: [
-    { x: 500, y: 100 },
-    { x: 300, y: 450 },
-    { x: 700, y: 450 },
-    { x: 490, y: 60 },
-    { x: 260, y: 450 },
-    { x: 710, y: 450 },
+    { x: 50, y: 50 },
+    { x: 50, y: 600 },
+    { x: 700, y: 600 },
   ],
   middleLine: [300, 300, 700, 300],
   bottomLine: [300, 450, 700, 450],
+  circleOnePoints: { x: 290, y: 250 },
+  circleTwoPoints: { x: 522, y: 450 },
 };
 
 export default AppState;
