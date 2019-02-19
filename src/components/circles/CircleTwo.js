4d76063b6e974762e4588519b2c0b140371f32a6
azizmashkour <azizmashkour@gmail.com>
2019-02-19 16:59:13 +0100

    feat: create circle one and two components

---
+++ src/components/circles/CircleTwo.js
@@ -0,0 +1,97 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Line,
+  Group,
+  Circle,
+  Text,
+} from 'react-konva';
+
+const CircleTwo = ({
+  points,
+  lineStrokeWidth,
+  stroke,
+  strokeWidth,
+  shadowBlur,
+  circleTwoPoints,
+  themeColor,
+  handleMouseEnter,
+  handleMouseLeave,
+  fontSize,
+}) => (
+  <Fragment>
+    <Group>
+      <Circle
+        x={circleTwoPoints.x}
+        y={circleTwoPoints.y}
+        radius={10}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        shadowBlur={shadowBlur}
+        fill={themeColor}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      <Circle
+        x={points[0].x}
+        y={circleTwoPoints.y}
+        radius={10}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        shadowBlur={shadowBlur}
+        fill={themeColor}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      <Line
+        points={
+          [
+            points[0].x,
+            circleTwoPoints.y,
+            circleTwoPoints.x,
+            circleTwoPoints.y,
+          ]
+        }
+        stroke={themeColor}
+        strokeWidth={lineStrokeWidth}
+      />
+      <Text
+        x={points[0].x - 40}
+        y={circleTwoPoints.y - 10}
+        text="F"
+        fontSize={fontSize}
+        fill={themeColor}
+      />
+      <Text
+        x={circleTwoPoints.x + 20}
+        y={circleTwoPoints.y - 10}
+        text="G"
+        fontSize={fontSize}
+        fill={themeColor}
+      />
+    </Group>
+  </Fragment>
+);
+
+CircleTwo.propTypes = {
+  node: PropTypes.shape({
+    A: PropTypes.string.isRequired,
+    B: PropTypes.string.isRequired,
+    C: PropTypes.string.isRequired,
+  }).isRequired,
+  points: PropTypes.arrayOf(PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+  })).isRequired,
+  shadowBlur: PropTypes.number.isRequired,
+  strokeWidth: PropTypes.number.isRequired,
+  lineStrokeWidth: PropTypes.number.isRequired,
+  stroke: PropTypes.string.isRequired,
+  themeColor: PropTypes.string.isRequired,
+  circleTwoPoints: PropTypes.shape({}).isRequired,
+  handleMouseEnter: PropTypes.func.isRequired,
+  handleMouseLeave: PropTypes.func.isRequired,
+  fontSize: PropTypes.number.isRequired,
+};
+
+export default CircleTwo;
