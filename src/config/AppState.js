44ee4f27713ab5474c7a6651d1100f4addc0b64a
azizmashkour <azizmashkour@gmail.com>
2019-02-20 17:39:08 +0100

    fix: update default state params

--- src/config/AppState.js
+++ src/config/AppState.js
@@ -13,13 +13,13 @@ export const AppState = {
   },
   points: [
     { x: 50, y: 50 },
-    { x: 50, y: 600 },
-    { x: 700, y: 600 },
+    { x: 50, y: 500 },
+    { x: 700, y: 500 },
   ],
   middleLine: [300, 300, 700, 300],
   bottomLine: [300, 450, 700, 450],
-  circleOnePoints: { x: 290, y: 250 },
-  circleTwoPoints: { x: 522, y: 450 },
+  circleOnePoints: { x: 340, y: 250 },
+  circleTwoPoints: { x: 557, y: 400 },
   isDrawing: false, // in the process of drawing a shape
   isDrawingMode: false, // allow shapes to be drawn
 };
