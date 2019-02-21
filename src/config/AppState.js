19726384cb97c82fe84ec4c4ecb387e9c6263344
azizmashkour <azizmashkour@gmail.com>
2019-02-21 15:18:02 +0100

    fix: remove all unused initial state params

--- src/config/AppState.js
+++ src/config/AppState.js
@@ -12,14 +12,12 @@ export const AppState = {
     E: 'E',
   },
   points: [
-    { x: 50, y: 50 },
-    { x: 50, y: 500 },
+    { x: 100, y: 50 },
+    { x: 100, y: 500 },
     { x: 700, y: 500 },
   ],
-  middleLine: [300, 300, 700, 300],
-  bottomLine: [300, 450, 700, 450],
-  circleOnePoints: { x: 340, y: 250 },
-  circleTwoPoints: { x: 557, y: 400 },
+  circleOnePoints: { x: 368, y: 250 },
+  circleTwoPoints: { x: 565, y: 400 },
   isDrawing: false, // in the process of drawing a shape
   isDrawingMode: false, // allow shapes to be drawn
   circleKind: null,
